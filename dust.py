1ee2929216c10c0125d3c7757178ae9f00099816
Sunwoong Joo <gomida@gmail.com>
2018-01-22 11:11:14 +0900

    multiple station

--- dust.py
+++ dust.py
@@ -5,19 +5,15 @@ import datetime
 import requests
 import urllib
 
-def airkorea():
-	# DB QUERY
-	connection = db.connect(host=info['host'], user=info['user'], password=info['password'], database=info['database'])
-	cursor = connection.cursor()
-	#cursor.execute("CREATE TABLE air_dust (id timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(), pm10_0 smallint(6) NOT NULL, pm02_5 smallint(6) NOT NULL) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;ALTER TABLE air_dust ADD UNIQUE KEY id (id);", multi=True)
-	cursor.execute("SELECT * FROM air_dust ORDER BY id DESC LIMIT 100")
+def airkorea(stationName, cursor):
+	cursor.execute("SELECT timestamp FROM air_dust WHERE station LIKE '%s' ORDER BY timestamp DESC LIMIT 100" % (stationName))
 	db_rows = cursor.fetchall()
 
 	# WEB QUERY
 	web_query = "http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty?serviceKey="\
 					+ info['serviceKey']\
 					+ "&numOfRows=24&pageSize=24&pageNo=1&startPage=1&stationName="\
-					+ urllib.parse.quote_plus(info['stationName'])\
+					+ urllib.parse.quote_plus(stationName)\
 					+ "&dataTerm=DAILY&ver=1.3"
 	web_xml = requests.get(web_query).text
 	soup = BeautifulSoup(web_xml, 'html.parser')
@@ -35,7 +31,7 @@ def airkorea():
 			measure_stamp = datetime.datetime.strptime(datatime, '%Y-%m-%d %H:%M')
 
 		dup = False
-		for db_row in db_rows: 
+		for db_row in db_rows:
 			if db_row[0] == measure_stamp:
 				dup = True
 				break
@@ -43,8 +39,16 @@ def airkorea():
 		if dup == False:
 			pm10_0 = item.find('pm10value').text
 			pm02_5 = item.find('pm25value').text
-			cursor.execute("INSERT INTO air_dust(id, pm10_0, pm02_5) VALUES (%s, %s, %s)", (measure_stamp, pm10_0, pm02_5))
+			if pm10_0 == '-': pm10_0 = -1
+			if pm02_5 == '-': pm02_5 = -1
+			cursor.execute("INSERT INTO air_dust(timestamp, station, pm10_0, pm02_5) VALUES (%s, %s, %s, %s)", (measure_stamp, stationName, pm10_0, pm02_5))
 
-	connection.commit()
 
-airkorea()
+# DB QUERY
+connection = db.connect(host=info['host'], user=info['user'], password=info['password'], database=info['database'])
+cursor = connection.cursor()
+
+for stationName in info['stationName']:
+	airkorea(stationName, cursor)
+
+connection.commit()
